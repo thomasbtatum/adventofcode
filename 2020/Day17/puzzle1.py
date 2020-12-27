a48c7d809a2f1980df547f0407091ad1014849eb
Thomas Tatum <thomastatum@thomass-mbp.myfiosgateway.com>
2020-12-27 01:08:31 -0500

    part2 is working

--- 2020/Day17/puzzle1.py
+++ 2020/Day17/puzzle1.py
@@ -1,37 +1,13 @@
 
 def neighbors(point):
     ne = []
-    #9 of these
-    ne.append((point[0]-1, point[1]-1, point[2]-1))
-    ne.append((point[0], point[1]-1, point[2]-1))
-    ne.append((point[0]+1, point[1]-1, point[2]-1))
-    ne.append((point[0]-1, point[1], point[2]-1))
-    ne.append((point[0], point[1], point[2]-1))
-    ne.append((point[0]+1, point[1], point[2]-1))
-    ne.append((point[0]-1, point[1]+1, point[2]-1))
-    ne.append((point[0], point[1]+1, point[2]-1))
-    ne.append((point[0]+1, point[1]+1, point[2]-1))
-    
-    #9 of these
-    ne.append((point[0]-1, point[1]-1, point[2]+1))
-    ne.append((point[0], point[1]-1, point[2]+1))
-    ne.append((point[0]+1, point[1]-1, point[2]+1))
-    ne.append((point[0]-1, point[1], point[2]+1))
-    ne.append((point[0], point[1], point[2]+1))
-    ne.append((point[0]+1, point[1], point[2]+1))
-    ne.append((point[0]-1, point[1]+1, point[2]+1))
-    ne.append((point[0], point[1]+1, point[2]+1))
-    ne.append((point[0]+1, point[1]+1, point[2]+1))
 
-    ne.append((point[0]-1, point[1]-1, point[2]))
-    ne.append((point[0], point[1]-1, point[2]))
-    ne.append((point[0]+1, point[1]-1, point[2]))
-    ne.append((point[0]-1, point[1], point[2]))
-    #ne.append[(point[0], point[1], point[2))
-    ne.append((point[0]+1, point[1], point[2]))
-    ne.append((point[0]-1, point[1]+1, point[2]))
-    ne.append((point[0], point[1]+1, point[2]))
-    ne.append((point[0]+1, point[1]+1, point[2]))
+    for dx in [-1,0,1]:
+        for dy in [-1,0,1]:
+            for dz in [-1,0,1]:
+                for dw in [-1,0,1]:
+                    if dx!=0 or dy!=0 or dz!=0 or dw!=0:
+                        ne.append((point[0]+dx,point[1]+dy,point[2]+dz,point[3]+dw))
 
     return ne
 
@@ -43,7 +19,7 @@ for l in L:
     x = 0
     for c in l:
         if c == '#':
-            active.append((x,y,0))
+            active.append((x,y,0,0))
         x+=1
     y+=1
 
@@ -52,18 +28,20 @@ print(active)
 for k in range(6):
     addthese = []
     removethese = []
-    for x in range(-20, 20):
-        for y in range (-20,20):
-            for z in range(-20,20):
-                point = (x,y,z)
-                n = neighbors(point)
-                activen = len(set(n).intersection(set(active)))
-                if point in active:
-                    if not 2<=activen<=3:
-                        removethese.append(point)
-                else:
-                    if 3 == activen:
-                        addthese.append(point)
+    for x in range(-15, 15):
+        for y in range (-15,15):
+            for z in range(-8,8):
+                for w in range(-8,8):
+                    point = (x,y,z,w)
+                    print(k,point)
+                    n = neighbors(point)
+                    activen = len(set(n).intersection(set(active)))
+                    if point in active:
+                        if not 2<=activen<=3:
+                            removethese.append(point)
+                    else:
+                        if 3 == activen:
+                            addthese.append(point)
 
     for p in addthese:
         active.append(p)
